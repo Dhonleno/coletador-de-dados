6a59475e9626bebbf784e68dd79fbe1af276f741
Dhon Freitas <dhonlenofreitas@hotmail.com>
2024-02-26 23:43:33 -0300

    mycommit

---
+++ idenficador/project.py
@@ -0,0 +1,29 @@
+'''
+Autor: Dhonleno Freitas
+Versão 1.0
+Data: 26/02/2024
+
+'''
+# Boas vindas
+
+print ('Olá! seja bem vindo!')
+
+# Coleta de dados
+
+nome = input('Qual é o seu nome? ')
+idade = input ('Qual é a sua idade? ')
+sexo = input('Qual seu gênero? ')
+est_civ = input('Qual seu estado civil? ')
+cidade = input ('Você mora em qual cidade? ')
+profissao = input('Qual sua proissão? ')
+estado_atual = input ('Atualmente está empregado ou desempregado? ')
+
+
+dados = (f'{nome} tem {idade} anos de idade, pertence ao gênero {sexo}, atualmente está {est_civ}, reside na cidade de {cidade}, é {profissao} e está {estado_atual}.')
+
+print (dados)
+
+# Fim
+
+
+
